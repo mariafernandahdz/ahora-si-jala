1528ba7fd739a79f48b3f7759aafad24bda08ba8
mariafernandahdz <fernandahdzc@outlook.com>
2020-11-16 23:21:25 -0600

    tercer

---
+++ src/main/java/com/example/demo/ProyectoU2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoU2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoU2Application.class, args);
+	}
+
+}
